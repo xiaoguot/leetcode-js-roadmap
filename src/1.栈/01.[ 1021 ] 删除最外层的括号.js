0303d2a68efa010ee9d8e5220a13d989cdc4a99b
guotian <987510914@qq.com>
2022-03-28 19:08:37 +0800

    01

--- src/1.栈/01.[ 1021 ] 删除最外层的括号.js
+++ src/1.栈/01.[ 1021 ] 删除最外层的括号.js
@@ -8,31 +8,28 @@
  * @param {string} S
  * @return {string}
  */
-var removeOuterParentheses = function (S) {
-  const bracket = { '(': ')' }
-  const stack = []
-  let res = []
-  const result = []
-  for (let i = 0; i < S.length; i++) {
-    const val = S[i]
-    if (bracket[val]) { // 左括号
-      stack.push(val)
-    } else { // 右括号
-      stack.pop()
+var removeOuterParentheses = function (s) {
+  const stack = [];
+  const res = [];
+  for (let i = 0; i < s.length; i++) {
+    //如果入栈前栈为空，说明即将入栈的'('是最外层括号
+    if(s[i] === '(' && stack.length === 0) {
+      stack.push('(');
+      //如果入栈前栈不为空，说明即将入栈的'('是内层括号，push进res中
+    }else if(s[i] === '(' && stack.length !== 0) {
+      res.push('(');
+      stack.push('(');
     }
-    if (stack.length === 0) {
-      // 存在一组有效括号
-      res.push(val)
-      const cur = [...res]
-      cur.shift()
-      cur.pop()
-      result.push(cur)
-      res = []
-      continue
+
+    //遇到右括号，栈就弹出。如果弹出后栈不为空，说明是内层括号，push进res中
+    if (s[i] === ')') {
+      stack.pop();
+      if(stack.length !== 0 ) {
+        res.push(')');
+      }
     }
-    res.push(val)
   }
-  return result.flat().join('')
+  return res.join('');
 };
 
 // 测试用例
